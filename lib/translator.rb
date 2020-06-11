0d42aa40541b7ed72750a3c1a9a00546c5064f03
Jeff Adler <jeffm.adler@gmail.com>
2020-06-11 21:10:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -33,7 +33,7 @@ def get_english_meaning(filepath,japanese_emoticon)
   #     library[emotion][:japanese] == japanese_emoticon
   # end)
   # emotion_string = emotion[0]
-  emoticon = "Sorry, that is not a known emoticon."
+  emoticon = "Sorry, that emoticon was not found"
    library.each do |emotion, description|
      if library[emotion][:japanese] == japanese_emoticon
        emoticon = emotion.to_s
