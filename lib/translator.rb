df353ecf65a5f8dbdefb19bdb72b0a17453bc2e7
Jeff Adler <jeffm.adler@gmail.com>
2020-06-11 20:20:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,26 @@
 # require modules here
+  require 'yaml'
+  require 'pry'
 
-def load_library
+def load_library(filepath)
   # code goes here
+  temp_library = YAML.load_file(filepath)
+  library = {}
+  temp_library.each do |key,value|
+    library[key] = {
+        :english => value[0],
+        :japanese => value[1]
+        }
+  end
+  library
 end
+
 
 def get_japanese_emoticon
   # code goes here
 end
 
-def get_english_meaning
+def get_english_meaning(japanese_emoticon)
   # code goes here
+  load_library()
 end
