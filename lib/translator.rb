3a4384645f08e78592154118864efffbd9fde372
Jeff Adler <jeffm.adler@gmail.com>
2020-06-11 20:47:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -32,6 +32,7 @@ def get_english_meaning(filepath,japanese_emoticon)
   emotion = (library.detect do |emotion,description|
       library[emotion][:japanese] == japanese_emoticon
   end)
+  binding.pry
   # library.each do |emotion, description|
   #   if library[emotion][:japanese] == japanese_emoticon
   #     return emotion.to_s
